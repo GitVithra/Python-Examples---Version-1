6573c0a2f0921834cebdcb8b7287affb478de741
Pavithra Neerudi <99332810+GitVithra@users.noreply.github.com>
2022-02-15 00:45:30 +0530

    Add files via upload

---
+++ find the polyndram number.py
@@ -0,0 +1,8 @@
+def f(n):
+	a = (n[::-1])
+	if a==n:
+		print("enter the number is polyndrom")
+	else:
+		print("enter the number is not a polyndrom")
+n = input("enter the value to check the polyndrom:")
+f(n)
