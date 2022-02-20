92783587040fae6f70af34d89ebbfe4c06d9d32b
Pavithra Neerudi <99332810+GitVithra@users.noreply.github.com>
2022-02-20 12:54:34 +0530

    Add files via upload

---
+++ Access instance varaible in the instance method.py
@@ -0,0 +1,27 @@
+class Student:
+
+    def __init__(self,name,age):
+    	self.name = name
+    	self.age = age
+    def show(self):
+    	print('Name:', s.name, 'Age:', s.age)
+
+a = Student("pavithra",21)
+
+a.show()
+
+
+	# constructor
+    
+
+	# instance variable
+	    
+	# instance method access  instance variable
+
+    
+# create object
+
+
+
+# call instance method
+
