43e26f893ed3c4a6365e26761207d614237fe8cc
Pavithra Neerudi <99332810+GitVithra@users.noreply.github.com>
2022-02-21 16:45:17 +0530

    Add files via upload

---
+++ Write the default constructor.py
@@ -0,0 +1,15 @@
+class Student:
+	# default constructor 
+	def __init__(self):
+		print("Constructor Executed")
+# create  reference variable
+
+a = Student()
+a = Student()
+a = Student()
+a = Student()
+a = Student()
+a = Student()
+a = Student()
+a = Student()
+a = Student()
