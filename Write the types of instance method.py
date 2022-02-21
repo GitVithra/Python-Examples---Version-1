120c2eb2eedfcbbda296ae6e91efd8632978f7c2
Pavithra Neerudi <99332810+GitVithra@users.noreply.github.com>
2022-02-21 18:39:08 +0530

    Add files via upload

---
+++ Write the types of instance method.py
@@ -0,0 +1,9 @@
+class student:
+	def __init__(self):
+		self.name = "Pavithra"
+	def display(self):
+		self.age = 21
+s1=student()
+s1.display()
+s1.address="Sangareddy"
+print(s1.name,s1.age,s1.address)
